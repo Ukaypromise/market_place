12ce864f86790fa58d249236c0ae64e1f0acafaa
Ukaypromise <promiseukaimo@gmail.com>
2024-06-26 14:45:27 +0100

    Test User model

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_26_124648) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test " user with a valid email should be valid " do
+    user = User.new(email: "test@gmail.com", password_digest:"123456")
+    assert user.valid?
+
+  end
+
+  test " user with invalid email should invalid" do
+    user = User.new(email: "test.com", password_digest:"123456")
+    assert_not user.valid?
+  end
+
+  test "user with email already taken should be invalid" do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest:"123456")
+
+    assert_not user.valid?
+  end
+
+  test "user without password should be not be valid" do
+    user = User.new(email: "test@gmail.com", password_digest:nil)
+    assert_not user.valid?
+  end
 end
