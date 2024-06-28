3d737ef151cada698b7329cc35c2084dd20bef73
Ukaypromise <promiseukaimo@gmail.com>
2024-06-28 20:09:41 +0800

    Add endpoint for creating new users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -11,6 +11,28 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
-end
 
-# example.com/api/v1/users/1
+  test "should create a new user" do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: {
+        user: {
+          email: "test@test.com",
+          password: "password"
+        }
+      }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should not create a new user if email is taken" do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: {
+        user: {
+          email: @user.email,
+          password: "password"
+        }
+      }, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
+end
