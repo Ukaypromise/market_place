c163ab7663191fc8cac40318023b85c7152cb058
Ukaypromise <promiseukaimo@gmail.com>
2024-08-18 02:42:23 +0100

    Adds product serializer for better json output

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,18 +6,18 @@ class Api::V1::ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    render json: @products
+    render json: ProductSerializer.new(@products).serializable_hash
   end
 
   def show
-    render json: @product
+    render json: ProductSerializer.new(@product).serializable_hash
   end
 
   def create
     @product = current_user.products.build(product_params)
 
     if @product.save
-      render json: @product, status: :created
+      render json: ProductSerializer.new(@product).serializable_hash, status: :created
     else
       render json: {
         errors: @product.errors
@@ -27,7 +27,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      render json: @product
+      render json: ProductSerializer.new(@product).serializable_hash
     else
       render json: @product.errors, status: :unprocessable_entity
     end
