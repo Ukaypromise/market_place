a08cbdf7fcbb68531187ec5dcc4c2826a1d0b97a
Ukaypromise <promiseukaimo@gmail.com>
2024-08-18 02:36:22 +0100

    Adds user serializer for better json output

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem "bootsnap", require: false
 
 gem 'jwt'
 
+gem 'fast_jsonapi'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
 
   # Get /users/1
   def show
-    render json: @user
+    render json: UserSerializer.new(@user).serializable_hash
   end
 
   # POST /users
@@ -14,10 +14,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: {
-        token: JsonWebToken.encode(user_id: @user.id),
-        data: @user,
-      },
+      render json: UserSerializer.new(@user).serializable_hash,
       status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -28,7 +25,7 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: UserSerializer.new(@user).serializable_hash, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
