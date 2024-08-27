9cb888783d1a617cd7812b33bcf7bec580f47bed
Ukaypromise <promiseukaimo@gmail.com>
2024-08-27 15:48:13 +0100

    Add filter and search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,20 @@ class Product < ApplicationRecord
 
   validates :price, numericality: { greater_than_or_equal_to: 0}, presence: true
   validates :title, presence: true
+
+  scope :filter_by_title, lambda { |query|
+    where('lower(title) LIKE ?', "%#{query.downcase}%")
+  }
+
+  scope :above_or_equal_to_price, ->(price){
+    where('price >= ?', price).order(:price)
+  }
+
+  scope :below_or_equal_to_price, ->(price){
+    where('price <= ?', price).order(:price)
+  }
+
+  scope :recent, lambda {
+    order(:updated_at)
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test"should filter products by name" do
+    assert_equal 2, Product.filter_by_title('MacBook').count
+  end
+  test" should filter products by name and sort the products" do
+    assert_equal [products(:four), products(:two)], Product.filter_by_title('MacBook').sort
+  end
+
+  test "should filter products by the price and sort them" do
+    assert_equal [products(:four), products(:two)], Product.above_or_equal_to_price(2000).sort
+  end
+
+  test "should filter products lower and sort them" do
+    assert_equal [products(:three), products(:one)], Product.below_or_equal_to_price(2000).sort
+  end
+
+  test "should sort product by the most recent" do
+    products(:two).touch
+    assert_equal [products(:one),products(:three), products(:four), products(:two)], Product.recent.to_a
+  end
+
 end
