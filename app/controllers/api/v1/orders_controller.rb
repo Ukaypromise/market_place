fdba3d4dbf9bb7ca812e2052a422b1890a4e2866
Ukaypromise <promiseukaimo@gmail.com>
2024-08-28 00:47:47 +0100

    Add create action for orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -13,4 +13,20 @@ class Api::V1::OrdersController < ApplicationController
       head 404
     end
   end
+
+  def create
+    @order = current_user.orders.build(order_params)
+
+    if @order.save
+      render json: @order, status: 201
+    else
+      render json:{ errors: @order.errors}, status: 422
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:total, product_ids: [])
+  end
 end
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @order = orders(:one)
+    @order_params = {
+      order: { product_ids: [products(:one).id, products(:two).id],
+                total: 4
+    }
+    }
   end
 
   test"should not allow orders if user not login"do
@@ -37,5 +42,21 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @order.products.first.title, include_product_attr['title']
   end
 
+  test "should not allow user to create an order if not logged in" do
+    assert_no_difference('Order.count') do
+      post api_v1_orders_url, params: @order_params, as: :json
+    end
+    assert_response :unauthorized
+  end
+
+  test "should create an order with two products" do
+    assert_difference('Order.count', 1) do
+      post api_v1_orders_url, params: @order_params,
+      headers: {
+        Authorization: JsonWebToken.encode(user_id: @order.user_id)
+      }, as: :json
+    end
+    assert_response :created
+  end
 
 end
